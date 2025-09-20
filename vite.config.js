b2fd76e380d7903f71e049aea449eca136f1edf4
ferhatisyapanys <76550525+ferhatisyapanys@users.noreply.github.com>
2025-09-20 13:26:17 +0300

    Vite'ı mevcut HTML dosyalarını serve edecek şekilde düzenle

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        contact: './contact.html'
+      }
+    }
+  },
+  server: {
+    open: true
+  }
+})
